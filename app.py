9e88d89e1f856640d4e4f12dbe574ae5b9f8c817
sergenp <sergen12343@gmail.com>
2020-08-14 14:20:30 +0300

    changed the name of server.py to app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from api.Resources import Book, Author, Category, Publisher
 # need to import models to initialize database using create_all method
 from models.Models import CategoryModel, AuthorModel, BookModel, PublisherModel
 
+
 db.create_all()
 app_configs = config.get('app_configs')
 app.config['UPLOAD_FOLDER'] = os.path.join(*config.get("file").get("path"))
@@ -17,10 +18,12 @@ api.add_resource(Author, '/authors/<int:author_id>', '/authors')
 api.add_resource(Category, '/categories/<int:category_id>', '/categories')
 api.add_resource(Publisher, '/publishers/<int:publisher_id>', '/publishers')
 
+
 @app.route('/files/<path:filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+
 if __name__ == '__main__':
-    app.run(debug=config.get('app_configs').get('debug_mode'))
+    app.run(debug=app_configs.get('debug_mode'), port=app_configs.get('port'))
