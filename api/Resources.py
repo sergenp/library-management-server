1911fd7d74397a970bef102549801e06628f7073
sergenp <sergen12343@gmail.com>
2020-08-14 14:24:30 +0300

    Updated Author model, add description column to BookModel

--- api/Resources.py
+++ api/Resources.py
@@ -11,33 +11,44 @@ from models.Models import CategoryModel, AuthorModel, BookModel, PublisherModel,
 
 
 ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+CONFIG = toml.load("./config.toml")
+UPLOAD_FOLDER = "/".join(CONFIG.get("file").get("path"))
 
-config = toml.load("./config.toml")
-UPLOAD_FOLDER = "/".join(config.get("file").get("path"))
 
 def allowed_image(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
 
+def date_type(x): return datetime.strptime(x, '%d/%m/%Y')
+
 
 class Author(Resource):
     def get(self, author_id):
-        author = AuthorModel.query.get_or_404(author_id, description="Author is not found")
-        return {'data' : {'author' : author.to_dict()}}, 200
+        author = AuthorModel.query.get_or_404(
+            author_id, description="Author is not found")
+        return {'data': {'author': author.to_dict()}}, 200
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str, help="name field is required")
+        parser.add_argument(
+            'name', type=str, help="Name field is required", required=True)
+        parser.add_argument(
+            'about', type=str, help="You need to provide an about for the author", required=True)
+        parser.add_argument('birth_date', type=date_type, default=None)
+        parser.add_argument('death_date', type=date_type, default=None)
         data = parser.parse_args()
-        author = AuthorModel(name=data['name'])
+        author = AuthorModel(
+            name=data['name'], about=data['about'], birth_date=data['birth_date'], death_date=data['death_date'])
         db.session.add(author)
         db.session.commit()
-        return {'data' : { "author" : author.to_dict()}}, 201
+        return {'data': {"author": author.to_dict()}}, 201
+
 
 class Category(Resource):
     def get(self, category_id):
-        category = CategoryModel.query.get_or_404(category_id, description="Category is not found")
-        return {'data' : {'category' : category.to_dict()}}, 200
+        category = CategoryModel.query.get_or_404(
+            category_id, description="Category is not found")
+        return {'data': {'category': category.to_dict()}}, 200
 
     def post(self):
         parser = reqparse.RequestParser()
@@ -46,12 +57,14 @@ class Category(Resource):
         category = CategoryModel(name=data['name'])
         db.session.add(category)
         db.session.commit()
-        return {'data' : { "category" : category.to_dict()}}, 201
+        return {'data': {"category": category.to_dict()}}, 201
+
 
 class Publisher(Resource):
     def get(self, publisher_id):
-        publisher = PublisherModel.query.get_or_404(publisher_id, description="Publisher is not found")
-        return {'data' : {'publisher' : publisher.to_dict()}}, 200
+        publisher = PublisherModel.query.get_or_404(
+            publisher_id, description="Publisher is not found")
+        return {'data': {'publisher': publisher.to_dict()}}, 200
 
     def post(self):
         parser = reqparse.RequestParser()
@@ -60,44 +73,57 @@ class Publisher(Resource):
         publisher = PublisherModel(name=data['name'])
         db.session.add(publisher)
         db.session.commit()
-        return {'data' : { "publisher" : publisher.to_dict()}}, 201
+        return {'data': {"publisher": publisher.to_dict()}}, 201
+
 
 class Book(Resource):
     def get(self, book_id):
-        book = BookModel.query.get_or_404(book_id, description="Book is not found")
-        return {'data' : {'book' : book.to_dict()}}, 200
-    
+        book = BookModel.query.get_or_404(
+            book_id, description="Book is not found")
+        return {'data': {'book': book.to_dict()}}, 200
+
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str, help="name field is required", required=True)
-        parser.add_argument('author_id', type=int, help="author_id field is required", required=True)
-        parser.add_argument('category_id', type=int, help="category_id field is required", required=True)
-        parser.add_argument('published_date', type=str, help="published_date field is required", required=True)
-        parser.add_argument('book_cover', type=werkzeug.datastructures.FileStorage, location='files', help=f"You must provide a book cover image, allowed extensions are {','.join(ALLOWED_IMAGE_EXTENSIONS)}", required=True)
-        parser.add_argument('publisher_id', type=str, help="publisher_id field is required", required=True)
-        data = parser.parse_args()
+        parser.add_argument(
+            'name', type=str, help="Name field is required", required=True)
+        parser.add_argument('author_id', type=int,
+                            help="You must provide an author", required=True)
+        parser.add_argument('category_id', type=int,
+                            help="You must provide a category", required=True)
         
+        parser.add_argument('published_date', type=date_type, default=None,
+                            help="Date doesn't match day/month/year format")
+        parser.add_argument('book_cover', type=werkzeug.datastructures.FileStorage, location='files',
+                            help=f"You must provide a book cover image, allowed extensions are {','.join(ALLOWED_IMAGE_EXTENSIONS)}", required=True)
+        parser.add_argument('description', type=str, required=True,
+                            help='You must provide a book description')
+        parser.add_argument('publisher_id', type=str, default=None)
+        data = parser.parse_args()
+
+        # these values might be None, we need to declare the variables to avaid ReferencedBeforeAssignment exception
+        publisher = data['publisher_id']
+        published_date = data['published_date']
+
         if allowed_image(data['book_cover'].filename):
-            filename = f"{uuid.uuid1()}.{data['book_cover'].filename.split('.')[1]}" 
+            filename = f"{uuid.uuid1()}.{data['book_cover'].filename.split('.')[1]}"
         else:
-            return {'message' : f"Given file cant be processed,allowed extensions are {','.join(ALLOWED_IMAGE_EXTENSIONS)}"}, 400
+            return {'message': f"Given file cant be processed,allowed extensions are {','.join(ALLOWED_IMAGE_EXTENSIONS)}"}, 400
 
-        author = AuthorModel.query.filter_by(id=data['author_id']).first_or_404(description=f"Author with id {data['author_id']} is not found")
-        category = CategoryModel.query.filter_by(id=data['category_id']).first_or_404(description=f"Category with id {data['category_id']} is not found")
-        publisher = PublisherModel.query.filter_by(id=data['publisher_id']).first_or_404(description=f"Publisher with id {data['publisher_id']} is not found")
-  
-        try:
-            published_date = datetime.strptime(data['published_date'], '%d/%m/%Y')
-        except ValueError:
-            return {'message' : 'Given date is not in the format of %d/%m/%Y'}, 400
+        author = AuthorModel.query.filter_by(id=data['author_id']).first_or_404(
+            description=f"Author with id {data['author_id']} is not found")
+        category = CategoryModel.query.filter_by(id=data['category_id']).first_or_404(
+            description=f"Category with id {data['category_id']} is not found")
 
-        new_book = BookModel(name=data['name'], published_date=published_date, author=author, category=category, book_cover=filename, publisher=publisher)
+        if publisher:
+            publisher = PublisherModel.query.filter_by(id=publisher).first_or_404(
+                description=f"Publisher with id {publisher} is not found")
+
+        new_book = BookModel(name=data['name'], published_date=published_date, description=data['description'],
+                             author=author, category=category, book_cover=filename, publisher=publisher)
         try:
             db.session.add(new_book)
             db.session.commit()
             data['book_cover'].save(os.path.join(UPLOAD_FOLDER, filename))
-            return {'data' : {'book': new_book.to_dict()}}, 201
+            return {'data': {'book': new_book.to_dict()}}, 201
         except IntegrityError as e:
-            return {'message' : 'Book already is in the database'}, 400
-
-
+            return {'message': 'Book already is in the database'}, 400
--- models/Models.py
+++ models/Models.py
@@ -1,12 +1,19 @@
 from db.database import db
 from sqlalchemy_serializer import SerializerMixin
 
+
 class AuthorModel(db.Model, SerializerMixin):
+    datetime_format = '%d/%m/%Y'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
+    about = db.Column(db.String, nullable=False)
+    birth_date = db.Column(db.DateTime, nullable=True)
+    death_date = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
         return '<Author %r>' % self.name
+
 
 class PublisherModel(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,10 +22,11 @@ class PublisherModel(db.Model, SerializerMixin):
     def __repr__(self):
         return '<Publisher %r>' % self.name
 
+
 class CategoryModel(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
-    
+
     def __repr__(self):
         return '<Category %r>' % self.name
 
@@ -30,12 +38,16 @@ class BookModel(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     published_date = db.Column(db.DateTime, nullable=True)
     book_cover = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String, nullable=False)
     author = db.relationship("AuthorModel")
     category = db.relationship("CategoryModel")
     publisher = db.relationship("PublisherModel")
-    author_id = db.Column(db.Integer, db.ForeignKey('author_model.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category_model.id'), nullable=False)
-    publisher_id = db.Column(db.Integer, db.ForeignKey('publisher_model.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey(
+        'author_model.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        'category_model.id'), nullable=False)
+    publisher_id = db.Column(db.Integer, db.ForeignKey(
+        'publisher_model.id'), nullable=False)
 
     def __repr__(self):
         return '<Book %r>' % self.name
