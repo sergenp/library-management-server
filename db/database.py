1360af9c95c311ef99020572d59f2560e577a7ed
sergenp <sergen12343@gmail.com>
2020-08-14 14:22:44 +0300

    add flask migrate

--- db/database.py
+++ db/database.py
@@ -1,10 +1,13 @@
 import toml
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask import Flask
 
 config = toml.load('./config.toml')
 app = Flask(config.get("app_name", __name__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)),*config.get("database").get("path"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), *config.get("database").get("path"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
